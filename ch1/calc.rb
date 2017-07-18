b9acd06e13bff60ad882f520b1045fbdc43e08b8
mokaymokay <mokay@Christophers-MacBook-Pro.local>
2017-07-17 17:37:21 -0700

    Add Chapter 1

---
+++ ch1/calc.rb
@@ -0,0 +1,10 @@
+puts 365 * 24
+# 365 days/year * 24 hour/day
+puts 10 * 365 * 24 * 60
+# 10 years * 365 days/year * 24 hours/day * 60 min/hour
+puts 25 * 365 * 24 * 60 * 60
+# age 25 * 365 days/year * 24 hours/day * 60 min/hour * 60 seconds/min
+puts (81 - 5) * 52 * 5
+# average lifespan minus toddler years * 52 weeks/year * 5 per week
+puts 1246000000 / 60 / 60 / 24 / 365
+# 1246mil seconds / 60 seconds/min / 60 min/hour / 24 hours/day / 365 days/year
