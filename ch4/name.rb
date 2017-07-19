7d9599bb19ae182fe6eac9568f30e2c12d5f2a5e
Kay Mok <kaymok3@gmail.com>
2017-07-19 14:01:23 -0700

    Add prompts for full name and print greeting.

---
+++ ch4/name.rb
@@ -0,0 +1,7 @@
+puts 'What\'s your first name?'
+firstName = gets.chomp
+puts 'What\'s your middle name?'
+middleName = gets.chomp
+puts 'What\'s your last name?'
+lastName = gets.chomp
+puts 'Hello, ' + firstName.to_s + ' ' + middleName.to_s + ' ' + lastName.to_s + '.'
