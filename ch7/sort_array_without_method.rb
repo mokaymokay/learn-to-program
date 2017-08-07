d82b5fe2048653a648dd1a37870fe066b1de83a2
Kay Mok <kaymok3@gmail.com>
2017-08-07 15:54:23 -0400

    Add instructions for Chapter 7 Exercise 2

---
+++ ch7/sort_array_without_method.rb
@@ -0,0 +1,3 @@
+# This program allows the user to type in as many words as s/he wishes,
+# one word per line, until pressing Enter on an empty line; then prints
+# the words in alphabetical order WITHOUT using the "sort" method.
