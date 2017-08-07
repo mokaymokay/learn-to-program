060e57af4d4afdea4cdff9df185ce5195f145b4d
Kay Mok <kaymok3@gmail.com>
2017-08-07 15:15:37 -0400

    Add program to sort words inputted by user alphabetically for Chapter 7 Exercise 1

--- ch7/sort_array.rb
+++ ch7/sort_array.rb
@@ -1,3 +1,10 @@
 # This program allows the user to type in as many words as s/he wishes,
 # one word per line, until pressing Enter on an empty line; then prints
 # the words in alphabetical order.
+
+puts "Type in as many words as you want, one word per line, pressing Enter on an empty line when you're done. The program will sort the list in an alphabetical order."
+array = []
+while (word = gets.chomp) != ""
+  array.push word
+end
+puts array.sort
