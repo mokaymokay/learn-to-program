b9b36023fb65d7505c47724532a704dc7a51d7cd
Kay Mok <kaymok3@gmail.com>
2017-08-06 22:52:53 -0400

    Add exercise prompt, cut unnecessary lines, and fix style for Chapter 6 Exercises 2 and 3.

--- ch6/deaf_grandma.rb
+++ ch6/deaf_grandma.rb
@@ -1,19 +1,19 @@
-#Grandma is deaf!
+# Grandma is pretty much deaf. She can only hear you if you shout (type in caps).
+# Even if you shout, she'd respond in gibberish saying "NOT SINCE [random year
+# from 1930-1950]". She will only go away if you shout "BYE" 3 times in a row.
 
 puts 'HELLO?? IT\'S YOUR GRANDMAMA!'
-count = 0
-while count < 3
+bye_count = 0
+while bye_count < 3
   if (reply = gets.chomp) != reply.upcase
-  puts 'HUH?!  SPEAK UP, SONNY!'
-  count = 0
+    puts 'HUH?!  SPEAK UP, SONNY!'
+    bye_count = 0
   elsif reply == "BYE"
-  count = (count + 1)
-  puts 'I MISSED TALKING TO YOU, SONNY!'
+    bye_count += 1
+    puts 'I MISSED TALKING TO YOU, SONNY!'
   else
-  puts 'NO, NOT SINCE ' + rand(1930..1950).to_s + '!'
-  count = 0
+    puts 'NO, NOT SINCE ' + rand(1930..1950).to_s + '!'
+    bye_count = 0
   end
-if count == 3
+end
 puts 'GOOD BYE, DEAR!'
-end
-end
