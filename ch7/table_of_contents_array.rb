2d092e0be0033bf4a106d4b95b9eb46d27f27063
Kay Mok <kaymok3@gmail.com>
2017-08-08 22:59:11 -0400

    Store Table of Contents data in array and display with visual formatting

--- ch7/table_of_contents_array.rb
+++ ch7/table_of_contents_array.rb
@@ -1,2 +1,20 @@
 # Rewrite the Table of Contents program from Chapter 5 Exercise 3
 # by storing text in an array.
+
+table = [
+  'Table of Contents',
+  'Chapter 1:  Numbers',
+  'page 1',
+  'Chapter 2:  Letters',
+  'page 72',
+  'Chapter 3:  Variables',
+  'page 118'
+]
+
+lineWidth = 50
+half_lineWidth = lineWidth/2
+puts table[0].center lineWidth
+puts
+puts table[1].ljust(half_lineWidth) + table[2].rjust(half_lineWidth)
+puts table[3].ljust(half_lineWidth) + table[4].rjust(half_lineWidth)
+puts table[5].ljust(half_lineWidth) + table[6].rjust(half_lineWidth)
