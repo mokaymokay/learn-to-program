6d5629bddf537f32655dc8096a3da5e997bb1d52
Kay Mok <kaymok3@gmail.com>
2017-08-30 15:11:12 -0400

    Add prompt to englishNumber exercise with expansion to return thousands

---
+++ ch8/englishNumber_thousand.rb
@@ -0,0 +1,3 @@
+# This method takes an integer written in digits and returns its English
+# version, i.e. it turns 22 into twenty-two. It will return 'one thousand'
+# instead of 'ten hundred' and 'ten thousand' instead of 'one hundred hundred'.
