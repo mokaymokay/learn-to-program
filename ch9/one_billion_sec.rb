63031349c537b22e0ddc85ef33f53c56c8fe000d
Kay Mok <kaymok3@gmail.com>
2017-09-01 11:02:56 -0400

    Add program to find out when I will be one billion seconds old

--- ch9/one_billion_sec.rb
+++ ch9/one_billion_sec.rb
@@ -1,1 +1,4 @@
 # This program will find out when I will or did turn one billion seconds old.
+
+puts "I will turn one billion seconds old at " + (Time.mktime(1991, 12, 10,
+  23, 00) + 1000000000).to_s
