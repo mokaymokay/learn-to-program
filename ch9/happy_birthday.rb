00a5e75cf0054c133207cc0abb6f8a04d9a4254e
Kay Mok <kaymok3@gmail.com>
2017-09-01 11:58:53 -0400

    Add program to ask for user's birthday and generate number of 'spanks' with age

--- ch9/happy_birthday.rb
+++ ch9/happy_birthday.rb
@@ -1,2 +1,19 @@
 # Ask users when they were born to and give them 'SPANK!'s for each
 # birthday they have had.
+
+puts 'What year were you born?'
+year = gets.chomp
+puts 'What month were you born?'
+month = gets.chomp
+puts 'What day were you born?'
+day = gets.chomp
+
+now = Time.new
+secondsAlive = Time.new - Time.mktime(year, month, day)
+birthdayInYears = secondsAlive/(60*60*24*365)
+
+puts "You\'re #{birthdayInYears.to_i} years old!"
+
+(birthdayInYears.to_i).times do
+  puts 'SPANK!'
+end
