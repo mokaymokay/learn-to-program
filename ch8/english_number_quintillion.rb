9d05be0de4ce93ca3a99d5c4818c10b5ad7d1e3b
Kay Mok <kaymok3@gmail.com>
2017-09-07 16:12:57 -0400

    Add comments and refine wording of some methods

--- ch8/english_number_quintillion.rb
+++ ch8/english_number_quintillion.rb
@@ -1,7 +1,8 @@
 # The method takes in an integer as an argument and outputs it in English, up
-# to the quintillionth place.
+# to the quintillionth place. It can be further expanded by adding data to the
+# big_numbers hash and separation_mapping array.
 
-# eg number = 12345678901234567890
+# e.g. number = 12345678901234567890
 # english_number(number)
 # => 'twelve quintillion three hundred forty-five quadrillion six hundred
 # seventy-eight trillion nine hundred one billion two hundred thirty-four
@@ -63,6 +64,7 @@ def english_number(number)
   return num_string
 end
 
+# Adds suffix acccording to number of places
 def num_to_suffix(number)
   big_numbers = {
     'quintillion' => (18..20).to_a,
@@ -83,33 +85,35 @@ def num_to_suffix(number)
   return correct_string
 end
 
+# Breaks number into sets of 3 (except for the hundredth place) by number of places
+# e.g. a 20-digit number would first break apart at the 18th place
 def break_into_first_suffix(number)
-  seperation_mapping = [
+  separation_mapping = [
     [2,3],[3,4,5,6],[6,7,8,9],[9,10,11,12],[12,13,14,15],[15,16,17,18],[18,19,20,21]
   ]
 
   break_point = nil
   return number if number.to_s.length <= 2
-  seperation_mapping.each do |set|
+  separation_mapping.each do |set|
     if set.include?(number.to_s.length)
       break_point = set[0]
       break
     end
   end
 
+# Breaks set further into parts for recursion
   solution = number.to_s.split('')
   break_point.times do
     solution.pop
   end
 
-  a = number.to_s
-  a.slice!(solution.join)
+  whats_left = number.to_s
+  whats_left.slice!(solution.join)
 
-  return [solution.join.to_i, a.to_i]
+  return [solution.join.to_i, whats_left.to_i]
 end
 
 puts english_number(1234)
-puts english_number(123456)
 puts english_number(123456)
 puts english_number(1234567)
 puts english_number(1234567890)
