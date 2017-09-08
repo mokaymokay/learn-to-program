492fcbde973f5ecb3793663895020065a5e61ee5
Kay Mok <kaymok3@gmail.com>
2017-09-08 17:36:13 -0400

    Improve format and increase versatility by comparing to current time

--- ch9/one_billion_sec.rb
+++ ch9/one_billion_sec.rb
@@ -1,4 +1,16 @@
 # This program will find out when I will or did turn one billion seconds old.
 
-puts "I will turn one billion seconds old at " + (Time.mktime(1991, 12, 10,
-  23, 00) + 1000000000).to_s
+t = (Time.mktime(1991, 12, 10, 23, 00) + 1000000000)
+
+def one_billion_sec(t)
+  period = ""
+  t.hour < 12 ? period = " AM" : " PM"
+  return "one billion seconds old on " + t.month.to_s + "/" + t.day.to_s + "/" +
+  t.year.to_s + " at " + t.hour.to_s + ":" + t.min.to_s + period.to_s
+end
+
+if Time.now < t
+  puts "I will turn #{one_billion_sec(t).to_s}."
+else
+  puts "I turned #{one_billion_sec(t).to_s}."
+end
