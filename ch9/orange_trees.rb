9202dc6c105d189baac6994506a8b84307bb3e9a
Kay Mok <kaymok3@gmail.com>
2017-09-12 18:13:11 -0400

    Add program to create the OrangeTree class and an instance of that class

--- ch9/orange_trees.rb
+++ ch9/orange_trees.rb
@@ -1,9 +1,176 @@
 # Make an OrangeTree class that contains a height method which returns its height,
-# and a oneYearPasses method, which, when called, ages the tree one year. Each year
+# and a one_year_passes method, which, when called, ages the tree one year. Each year
 # the tree grows taller, and after some number of years, the tree should die.
 # It only starts producing fruits after a few years of growing, and older trees
-# produce more each year than younger trees. Also, create methods countTheOranges
-# (which returns the number of oranges on the tree) and pickAnOrange (which reduces
-# the @orangeCount by one and returns a string telling the user how delicious the
+# produce more each year than younger trees. Also, create methods count_the_oranges
+# (which returns the number of oranges on the tree) and pick_an_orange (which reduces
+# the @orange_count by one and returns a string telling the user how delicious the
 # orange was, or else it just says that there are no more oranges to pick this
 # year). Any oranges that aren't picked fall off before the next year.
+
+class OrangeTree
+
+  def initialize
+    @height = 0
+    @age = 0
+    @orange_count = 0
+
+    puts "You planted an orange tree!"
+  end
+
+  def age
+    puts "Your tree is #{@age} years old." unless @age > 100
+    one_year_passes
+  end
+
+  def count_the_oranges
+    puts "There are #{@orange_count} oranges on the #{@height}-feet-tall tree."
+  end
+
+  def pick_an_orange
+    if @orange_count > 0
+      @orange_count -= 1
+      puts "You picked an orange. Yum! It's very succulent."
+    else
+      puts "There aren't any oranges on your tree. Please wait until the next season."
+    end
+  end
+
+  def one_year_passes
+    @orange_count = 0
+    if @age <= 100
+      @age += 1
+    end
+    if @height < 30
+      @height += 3
+    end
+    if @age > 3
+      @orange_count += @height * 10
+    end
+    if @age > 100
+      puts "Your tree has gracefully retired after providing you with many delicious oranges."
+      exit
+    end
+  end
+end
+
+tree = OrangeTree.new
+tree.age
+tree.pick_an_orange
+tree.age
+tree.count_the_oranges
+tree.age
+tree.age
+tree.count_the_oranges
+tree.age
+tree.pick_an_orange
+tree.age
+tree.age
+tree.count_the_oranges
+tree.age
+tree.pick_an_orange
+tree.age
+tree.age
+tree.age
+tree.count_the_oranges
+tree.age
+tree.pick_an_orange
+tree.pick_an_orange
+tree.pick_an_orange
+tree.age
+tree.pick_an_orange
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.pick_an_orange
+tree.count_the_oranges
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.count_the_oranges
+tree.pick_an_orange
+tree.pick_an_orange
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.count_the_oranges
+tree.pick_an_orange
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
+tree.age
