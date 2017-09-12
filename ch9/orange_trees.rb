f7a3fad60b9a1c5d34b4e4b1ad137a2348e77c55
Kay Mok <kaymok3@gmail.com>
2017-09-12 14:32:59 -0400

    Add prompt to create class for orange trees

---
+++ ch9/orange_trees.rb
@@ -0,0 +1,9 @@
+# Make an OrangeTree class that contains a height method which returns its height,
+# and a oneYearPasses method, which, when called, ages the tree one year. Each year
+# the tree grows taller, and after some number of years, the tree should die.
+# It only starts producing fruits after a few years of growing, and older trees
+# produce more each year than younger trees. Also, create methods countTheOranges
+# (which returns the number of oranges on the tree) and pickAnOrange (which reduces
+# the @orangeCount by one and returns a string telling the user how delicious the
+# orange was, or else it just says that there are no more oranges to pick this
+# year). Any oranges that aren't picked fall off before the next year.
