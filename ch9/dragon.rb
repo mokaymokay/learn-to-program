b2c6967fbdf618732fd20200b81f107216372134
Kay Mok <kaymok3@gmail.com>
2017-09-13 15:17:18 -0400

    Add user interaction with baby dragon

--- ch9/dragon.rb
+++ ch9/dragon.rb
@@ -5,120 +5,124 @@ class Dragon
   def initialize name
     @name = name
     @asleep = false
-    @stuffInBelly     = 10  # He's full.
-    @stuffInIntestine =  0  # He doesn't need to go.
+    @stuff_in_belly     = 10  # He's full.
+    @stuff_in_intestine =  0  # He doesn't need to go.
 
-    puts @name + ' is born.'
+    puts "#{@name} is born."
+  end
+
+  def list_of_commands
+    puts "Here are some things you can do with #{@name}:"
+    puts "feed, walk, put_to_bed, toss, rock"
   end
 
   def feed
-    puts 'You feed ' + @name + '.'
-    @stuffInBelly = 10
+    puts "You feed #{@name}."
+    @stuff_in_belly = 10
     passageOfTime
   end
 
   def walk
-    puts 'You walk ' + @name + '.'
-    @stuffInIntestine = 0
+    puts "You walk #{@name}."
+    @stuff_in_intestine = 0
     passageOfTime
   end
 
-  def putToBed
-    puts 'You put ' + @name + ' to bed.'
+  def put_to_bed
+    puts "You put #{@name} to bed."
     @asleep = true
     3.times do
       if @asleep
         passageOfTime
       end
       if @asleep
-        puts @name + ' snores, filling the room with smoke.'
+        puts "#{@name} snores, filling the room with smoke."
       end
     end
     if @asleep
       @asleep = false
-      puts @name + ' wakes up slowly.'
+      puts "#{@name} wakes up slowly."
     end
   end
 
   def toss
-    puts 'You toss ' + @name + ' up into the air.'
-    puts 'He giggles, which singes your eyebrows.'
+    puts "You toss #{@name} up in the air."
+    puts "It giggles, which singes your eyebrows."
     passageOfTime
   end
 
   def rock
-    puts 'You rock ' + @name + ' gently.'
+    puts "You rock #{@name} gently."
     @asleep = true
-    puts 'He briefly dozes off...'
+    puts "It briefly dozes off..."
     passageOfTime
     if @asleep
       @asleep = false
-      puts '...but wakes when you stop.'
+      puts "...but wakes when you stop."
     end
   end
 
   private
 
-  # "private" means that the methods defined here are
-  # methods internal to the object.  (You can feed
-  # your dragon, but you can't ask him if he's hungry.)
-
   def hungry?
-    # Method names can end with "?".
-    # Usually, we only do this if the method
-    # returns true or false, like this:
-    @stuffInBelly <= 2
+    @stuff_in_belly <= 2
   end
 
   def poopy?
-    @stuffInIntestine >= 8
+    @stuff_in_intestine >= 8
   end
 
   def passageOfTime
-    if @stuffInBelly > 0
+    if @stuff_in_belly > 0
       # Move food from belly to intestine.
-      @stuffInBelly     = @stuffInBelly     - 1
-      @stuffInIntestine = @stuffInIntestine + 1
+      @stuff_in_belly     = @stuff_in_belly     - 1
+      @stuff_in_intestine = @stuff_in_intestine + 1
     else  # Our dragon is starving!
       if @asleep
         @asleep = false
-        puts 'He wakes up suddenly!'
+        puts 'It wakes up suddenly!'
       end
-      puts @name + ' is starving!  In desperation, he ate YOU!'
+      puts "#{@name} is starving!  In desperation, it ate YOU!"
       exit  # This quits the program.
     end
 
-    if @stuffInIntestine >= 10
-      @stuffInIntestine = 0
-      puts 'Whoops!  ' + @name + ' had an accident...'
+    if @stuff_in_intestine >= 10
+      @stuff_in_intestine = 0
+      puts "Whoops! #{@name} had an accident..."
     end
 
     if hungry?
       if @asleep
         @asleep = false
-        puts 'He wakes up suddenly!'
+        puts "It wakes up suddenly!"
       end
-      puts @name + '\'s stomach grumbles...'
+      puts "#{@name}\'s stomach grumbles..."
     end
 
     if poopy?
       if @asleep
         @asleep = false
-        puts 'He wakes up suddenly!'
+        puts 'It wakes up suddenly!'
       end
-      puts @name + ' does the potty dance...'
+      puts "#{@name} does the potty dance..."
     end
   end
 
 end
 
-pet = Dragon.new 'Norbert'
-pet.feed
-pet.toss
-pet.walk
-pet.putToBed
-pet.rock
-pet.putToBed
-pet.putToBed
-pet.putToBed
-pet.putToBed
+puts "What do you wish to name your dragon?"
+name = gets.chomp.to_s
+pet = Dragon.new name
+pet.list_of_commands
+
+while true
+  command = gets.chomp.to_s.downcase
+  if command == "exit"
+    exit
+  elsif pet.respond_to?(command)
+    pet.send command
+  else
+    puts "Please enter a valid command or 'exit' if you wish to quit the program."
+    pet.list_of_commands
+  end
+end
