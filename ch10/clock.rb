230fa03fd7bd5efbf8857a9f4b4768b78c2665e4
Kay Mok <kaymok3@gmail.com>
2017-09-25 12:20:23 -0400

    Include the use of a block

--- ch10/clock.rb
+++ ch10/clock.rb
@@ -2,23 +2,24 @@
 # passed today. That way, if I were to pass in the block do puts 'DONG!' end,
 # it would chime (sort of) like a grandfather clock. Test your method out with
 # a few different blocks (including the one I just gave you). Hint: You can use
-# Time.now.hour to get the current hour. However, this returns a number between
+# Time.new.hour to get the current hour. However, this returns a number between
 # 0 and 23, so you will have to alter those numbers in order to get ordinary
 # clock-face numbers (1 to 12).
 
-class Fixnum
-  def grandfather_clock(number)
-    number.times do
-      puts "DONG!"
-    end
-  end
-end
-
-hours_passed = Time.now.hour
+def grandfather_clock(&block)
+  hours_passed = Time.new.hour
   if hours_passed > 12
     hours_passed -= 12
   else
     hours_passed
   end
+  hours_passed.times do
+    block.call
+  end
+end
 
-520.grandfather_clock(hours_passed)
+puts "Currently, it is " + Time.new.to_s + "."
+
+grandfather_clock do
+  puts "DONG!"
+end
